56116d3a05262c106d6ce51e15a2306165e0775f
ajlinhard <145815985+ajlinhard@users.noreply.github.com>
2025-03-07 10:12:53 -0500

    test unit testing

--- src/Book_API/__init__.py
+++ src/Book_API/__init__.py
@@ -1,4 +1,4 @@
+
 from .models import Book
 from .services.book_service import BookService
 from .resources.book_resources import BookListResource, BookResource
-from .app import create_app
--- src/Book_API/resources/book_resources.py
+++ src/Book_API/resources/book_resources.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 from flask_restful import Resource
-from services.book_service import BookService
+# There is an issue when running app.py vs pytest where the path requres no "." in front of the path and then requires 2 "." in front of the path
+from ..services.book_service import BookService
 
 class BookListResource(Resource):
     def __init__(self):
